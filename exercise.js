b6093be4870ed4038b0fb7794436f8fcc9957450
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-11-09 13:20:38 -0500

    practice

---
+++ exercise.js
@@ -0,0 +1,45 @@
+
+
+// function
+function hello(name) {
+    
+};
+
+// arrow function
+const hello = (name) => {
+
+};
+// variable that can be changed
+let messagediv = "";
+
+// variable that can't be changed
+const messageDiv = "";
+
+// variable that hold an object named animal(has 2 properties)
+
+// object
+let animals = {
+    name: 'Hermano',
+    hasFur: false,
+}
+
+// array
+let numbers = [1,2,3,4,5,6,7,8,];
+
+// array of courses
+let courses = [
+    {courseName: 'Intro to JS'},
+    {courseName: 'Intermediate JS'},
+    {courseName: 'Advanced JS'},
+];
+
+// for-of loop
+for (const course of courses) {
+    
+}
+
+// for loop
+for (let index = 0; index < courses.length; index++) {
+    const course = courses[index];
+    
+}
