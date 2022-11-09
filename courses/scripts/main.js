704c7af5f6cb9d59f29b2ceb7ae1bd6bd7def5ad
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-11-09 15:24:54 -0500

    added link to title

--- courses/scripts/main.js
+++ courses/scripts/main.js
@@ -3,22 +3,27 @@
 const courseTableBody = document.querySelector("#courseTable tbody");
 
 function loadCourseTableBody() {
-    fetch("http://localhost:8082/api/courses")
+  fetch("http://localhost:8082/api/courses")
     .then((response) => response.json())
     .then((courses) => {
-        courses.forEach((course) => {
-            let row = courseTableBody.insertRow(-1);
-            let cell1 = row.insertCell(0);
-            let cell2 = row.insertCell(1);
-            let cell3 = row.insertCell(2);
-            cell1.innerText = course.courseNum;
-            cell2.innerText = course.courseName;
-            cell3.innerText = course.dept;
-        });;
-    })
-}
+      courses.forEach((course) => {
+        let row = courseTableBody.insertRow(-1);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
 
+        cell1.innerText = course.courseNum;
+        cell3.innerText = course.dept;
+
+        const anchor = document.createElement('a');
+        anchor.innerText = course.courseName;
+        anchor.href = './details.html'
+        cell2.appendChild(anchor)
+
+      });
+    });
+}
 
 window.onload = () => {
-    loadCourseTableBody();
-}
+  loadCourseTableBody();
+};
