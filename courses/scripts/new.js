310ccc904bcece014ffe582263479d794e8350e7
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-11-16 13:35:37 -0500

    finished new course pg

--- courses/scripts/new.js
+++ courses/scripts/new.js
@@ -2,19 +2,41 @@
 
 const $q = (selector) => document.querySelector(selector);
 const courseNameInput = $q("#course-name-input");
-const courseNumberInput = $q("course-number-input");
-const courseDeptInput = $q("course-dept-input");
-const courseInstrutorInput = $q("course-instructor-input");
-const startDateInput = $q("course-startdate")
-const form = $q("form");
+const courseNumberInput = $q("#course-number-input");
+const courseDeptInput = $q("#course-dept-input");
+const courseInstrutorInput = $q("#course-instructor-input");
+const startDateInput = $q("#course-startdate-input");
+const numDaysInput = $q("#numDays-input");
+const messageParagraph = $q("#messageParagraph");
 
 function saveCourse(event) {
-    event.preventDefault():
+    event.preventDefault();
 
+    const courseData = {
+        dept: courseDeptInput.value,
+        courseNum: courseNumberInput.value,
+        courseName: courseNameInput.value,
+        instuctor: courseInstrutorInput.value,
+        startDate: startDateInput.value,
+        numDays: numDaysInput.value,
+    }
 
-
+    fetch("http://localhost:8888/courses/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify(courseData)
+    }).then(response => response.json())
+    .then(data => {
+        console.log(data)
+        window.location = "./courses.html";
+    })
+    .catch(error => {
+        console.log(error);
+        messageParagraph.innerText = "An unexpected error occured.";
+    })
 }
 
-window.onload = () => {
-    form.submit = saveCourse;
+window.onload = function() {
+    const form = $q("form");
+    form.onsubmit = saveCourse;
 }
